de0477ecf756514f2e6256c027f1919dbcadc843
Krishna Majgaonkar <krishhna123@gmail.com>
2022-01-18 17:36:19 +0100

    practice with element finders

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+it("should render same text passed into title prop for header component", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = screen.getByText(/my header/i);
+  expect(headingElements).toBeInTheDocument();
+});
+
+it("should render element based on role", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = screen.getByRole("heading", { name: "My Header" });
+  expect(headingElements).toBeInTheDocument();
+});
+
+it("should render element based on title", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = screen.getByTitle("Header");
+  expect(headingElements).toBeInTheDocument();
+});
+
+it("should render element based on test-id", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = screen.getByTestId("header-test");
+  expect(headingElements).toBeInTheDocument();
+});
+
+// FIND By
+it("should render same text passed into title prop for header component", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = await screen.findByText(/my header/i);
+  expect(headingElements).toBeInTheDocument();
+});
+
+// QueryBy
+
+it("should not render element which is not passed as a prop", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = await screen.queryByText(/dog/i);
+  expect(headingElements).not.toBeInTheDocument();
+});
+
+it("should render same text passed into title prop for header component", async () => {
+  render(<Header title="My Header" />);
+  const headingElements = await screen.getAllByRole("heading");
+  expect(headingElements.length).toBe(2);
+});
